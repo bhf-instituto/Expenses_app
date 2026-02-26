115af7a20343e412a9dcdda5eaab0337c7772a81
mariano bauhoffer <bauhoffermariano.instituto@gmail.com>
2026-02-26 19:00:54 -0300

    .

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_PROXY_TARGET = 'https://bbhhffexpensesapp.dpdns.org'
+const API_PROXY_TARGET = 'https://bbhhffexpensesapp.dpdns.org/expenses_api/'
 
 // https://vitejs.dev/config/
 export default defineConfig({
